730f6647845f6a59cf3f6ed94e3da349109f796a
Marios Galanis <MariosGalanis@outlook.com>
2020-11-08 12:28:55 +0200

    create Tab Navigation

---
+++ src/screens/Screens.tsx
@@ -0,0 +1,76 @@
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useContext, useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { ProfileContext } from './../context/AppContext';
+import Column from '../layout/Column';
+import { validateProfile } from './../validators/validateProfile';
+import ProfileScreen from './ProfileScreen';
+import { ScreensParamList } from './ScreensParamList';
+import SmsScreen from './SmsScreen';
+
+const Tab = createBottomTabNavigator<ScreensParamList>();
+
+export function Screens() {
+  const { profile, setProfile } = useContext(ProfileContext);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const profile = await AsyncStorage.getItem('profile');
+
+        if (profile !== null) {
+          setProfile(JSON.parse(profile));
+          setLoading(false);
+        }
+      } catch (error) {
+        alert('Προέκυψε κάποιο σφάλμα');
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (loading)
+    return (
+      <Column>
+        <ActivityIndicator size="large" />
+      </Column>
+    );
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={validateProfile(profile) ? 'SmsScreen' : 'ProfileScreen'}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, size }) => {
+            let iconName = '';
+
+            if (route.name === 'SmsScreen') {
+              iconName = 'ios-mail';
+            } else if (route.name === 'ProfileScreen') {
+              iconName = 'ios-person';
+            }
+
+            return (
+              <Ionicons
+                name={iconName}
+                size={size}
+                color={focused ? '#376996' : '#c4d5eb'}
+              />
+            );
+          },
+          tabBarLabel: () => null
+        })}>
+        <Tab.Screen name="SmsScreen" options={{ title: 'SMS' }} component={SmsScreen} />
+        <Tab.Screen
+          name="ProfileScreen"
+          options={{ title: 'Στοιχεία' }}
+          component={ProfileScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
