e97c65ac9d47370203f2fca054d9f8deb669ac5f
Marios Galanis <MariosGalanis@outlook.com>
2020-11-08 12:29:16 +0200

    import Provider and Screens to App

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  printWidth: 90
+};
--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ProfileProvider } from './src/context/AppContext';
+import { Screens } from './src/screens/Screens';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ProfileProvider>
+      <Screens />
+    </ProfileProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
