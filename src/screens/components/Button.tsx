f38c19dc06c2aa7eb2349d7c75c5955e5e9d4c47
Marios Galanis <MariosGalanis@outlook.com>
2020-11-08 13:17:59 +0200

    change button font family

--- src/screens/components/Button.tsx
+++ src/screens/components/Button.tsx
@@ -1,30 +1,42 @@
-import React, {ReactNode} from 'react'
-import { StyleSheet, Text, TouchableOpacity , StyleProp, FlexStyle, ViewStyle} from 'react-native';
+import React, { ReactNode } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  StyleProp,
+  FlexStyle,
+  ViewStyle,
+  TextStyle
+} from 'react-native';
 
 type PropTypes = {
-    children: ReactNode,
-    onPress: ( ) => void,
-    style?: StyleProp<FlexStyle | ViewStyle>
-}
-export function Button({children, onPress, style}: PropTypes) {
-    return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={[styles.buttonText, style]}>{children}</Text>
-        </TouchableOpacity>
-    )
+  children: ReactNode;
+  onPress: () => void;
+  style?: StyleProp<FlexStyle | ViewStyle>;
+};
+export function Button({ children, onPress, style }: PropTypes) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={[styles.buttonText, style]}>{children}</Text>
+    </TouchableOpacity>
+  );
 }
 
-const styles = StyleSheet.create({
-    button: {
-      backgroundColor: '#376996',
-      borderRadius: 50,
-      width: '100%',
-      paddingVertical: 15,
-      alignItems: 'center'
-    },
-    buttonText: {
-      fontSize: 18,
-      color: 'white'
-    }
-  });
-  
+type StyleProps = {
+  button: FlexStyle | ViewStyle;
+  buttonText: TextStyle;
+};
+const styles = StyleSheet.create<StyleProps>({
+  button: {
+    backgroundColor: '#376996',
+    borderRadius: 50,
+    width: '100%',
+    paddingVertical: 15,
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    fontFamily: 'Arial'
+  }
+});
